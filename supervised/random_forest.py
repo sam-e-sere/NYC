3eb5ce28ed05a1db07d514c6c134658dcf1702a1
liuzzisam <s.liuzzi17@studenti.uniba.it>
2023-06-06 16:25:47 +0200

    random_forest

--- supervised/random_forest.py
+++ supervised/random_forest.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import OrdinalEncoder
@@ -26,39 +26,37 @@ X.loc[:, categorical_features] = encoder.fit_transform(X[categorical_features])
 # Divisione del dataset in training set e test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-for crit in {"gini", "entropy", "log_loss"}:
-    print(f"Criterion: {crit}")
 
-    mean_train_score = []
-    mean_test_score = []
-    mean_test_p = []
-    mean_test_r = []
-    mean_test_f = []
+mean_train_score = []
+mean_test_score = []
+mean_test_p = []
+mean_test_r = []
+mean_test_f = []
 
-    for i in range(3, 26):
-        clf = DecisionTreeClassifier(max_depth=i, criterion=crit, random_state=0)
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        scoring = {'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall', 'f1': 'f1'}
-        cv_results = cross_validate(clf, X, y, cv=5, scoring=scoring, return_train_score=True)
-        mean_train_score.append(np.mean(cv_results['train_accuracy']))
-        mean_test_score.append(np.mean(cv_results['test_accuracy']))
-        mean_test_p.append(np.mean(cv_results['test_precision']))
-        mean_test_r.append(np.mean(cv_results['test_recall']))
-        mean_test_f.append(np.mean(cv_results['test_f1']))
+for i in range(3, 26):
+    clf = RandomForestClassifier(n_estimators=100, max_depth=i, criterion="entropy", random_state=0)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    scoring = {'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall', 'f1': 'f1'}
+    cv_results = cross_validate(clf, X, y, cv=5, scoring=scoring, return_train_score=True)
+    mean_train_score.append(np.mean(cv_results['train_accuracy']))
+    mean_test_score.append(np.mean(cv_results['test_accuracy']))
+    mean_test_p.append(np.mean(cv_results['test_precision']))
+    mean_test_r.append(np.mean(cv_results['test_recall']))
+    mean_test_f.append(np.mean(cv_results['test_f1']))
 
-    plt.plot(range(3, 26), mean_train_score, label="Training score")
-    plt.plot(range(3, 26), mean_test_score, label="Test score")
-    plt.xlabel("Tree Depth")
-    plt.ylabel("Score")
-    plt.legend()
-    #plt.savefig(f"{directory}tree_{crit}.png")
-    plt.show()
+plt.plot(range(3, 26), mean_train_score, label="Training score")
+plt.plot(range(3, 26), mean_test_score, label="Test score")
+plt.xlabel("Tree Depth")
+plt.ylabel("Score")
+plt.legend()
+#plt.savefig(f"{directory}tree_{crit}.png")
+plt.show()
 
-    print(f"Media test acc: {np.mean(mean_test_score)}")
-    print(f"Media test prec: {np.mean(mean_test_p)}")
-    print(f"Media test rec: {np.mean(mean_test_r)}")
-    print(f"Media test f-measure: {np.mean(mean_test_f)}")
+print(f"Media test acc: {np.mean(mean_test_score)}")
+print(f"Media test prec: {np.mean(mean_test_p)}")
+print(f"Media test rec: {np.mean(mean_test_r)}")
+print(f"Media test f-measure: {np.mean(mean_test_f)}")
 
 # Visualizzazione delle feature più importanti
 importances = clf.feature_importances_
